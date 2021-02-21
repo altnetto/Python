2155e8d74625e04d8cbf221fa6778a5e89e42d85
altnetto <altnetto@gmail.com>
2021-02-21 19:38:14 -0300

    Implemented the app factory pattern

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from TU101.app import create_app
 
+
 @pytest.fixture(scope="module")
 def app():
     return create_app()
--- test/test_app.py
+++ test/test_app.py
@@ -1,2 +1,10 @@
 def test_app_created(app):
     assert app.name == 'TU101.app'
+
+
+def test_config_is_loaded(config):
+    assert config['DEBUG'] is False
+
+
+def test_request_returns_404(client):
+    assert client.get('/url_que_nao_existe').status_code == 404
